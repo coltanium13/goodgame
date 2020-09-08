2bfc7c462b21b9a0f9f9b126165eda01ffb9ce48
Colton <colton@Coltons-MBP.home>
2020-09-07 20:20:23 -0700

    git inital commit

---
+++ models/Profile.js
@@ -0,0 +1,144 @@
+const mongoose = require('mongoose');
+
+//TODO: clean up un needed properties
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  company: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  skills: {
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  githubusername: {
+    type: String
+  },
+  games: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      status: {
+        type: String,
+        enum: ['playing', 'backlog', 'played'],
+        default: 'backlog',
+        required: true
+      },
+      imageUrl: {
+        type: String
+      },
+      description: {
+        type: String
+      },
+      notes: {
+        type: String
+      },
+      finishedDate: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      }
+    }
+  ],
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      company: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldofstudy: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('profile', ProfileSchema);
